f84986bd40d655c221e1795f05d203b03aa7eb61
Baptiste <bapmarti@gmail.com>
2022-07-16 20:20:11 +0200

    Updates

---
+++ precache-manifest.b74350fed6160d91a3578ea7a623afc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d39e2c46eedf799cc2e4",
+    "url": "/bmarti.github.io/static/css/main.a365e051.chunk.css"
+  },
+  {
+    "revision": "d39e2c46eedf799cc2e4",
+    "url": "/bmarti.github.io/static/js/main.32e2c150.chunk.js"
+  },
+  {
+    "revision": "c820f46030e4930672cb",
+    "url": "/bmarti.github.io/static/js/runtime~main.2fc52929.js"
+  },
+  {
+    "revision": "8ccdeb984cb8aea512b1",
+    "url": "/bmarti.github.io/static/js/2.fb29e010.chunk.js"
+  },
+  {
+    "revision": "9d173821cec86f4d7f4817a729b7d4b3",
+    "url": "/bmarti.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7752a748a2b2af04af2e3495f9d1d033.js"
+  "/bmarti.github.io/precache-manifest.b74350fed6160d91a3578ea7a623afc9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/bmarti.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
