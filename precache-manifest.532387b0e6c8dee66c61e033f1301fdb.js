aa6b6e2537cd5ec259ef3650a7c4b6192683d653
Baptiste <bapmarti@gmail.com>
2022-07-17 10:55:45 +0200

    Updates

---
+++ precache-manifest.532387b0e6c8dee66c61e033f1301fdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d39e2c46eedf799cc2e4",
+    "url": "/bmarti-portfolio/static/css/main.a365e051.chunk.css"
+  },
+  {
+    "revision": "d39e2c46eedf799cc2e4",
+    "url": "/bmarti-portfolio/static/js/main.32e2c150.chunk.js"
+  },
+  {
+    "revision": "97e134eff146cb0585cb",
+    "url": "/bmarti-portfolio/static/js/runtime~main.e52a0645.js"
+  },
+  {
+    "revision": "8ccdeb984cb8aea512b1",
+    "url": "/bmarti-portfolio/static/js/2.fb29e010.chunk.js"
+  },
+  {
+    "revision": "3892d9c05586111a6f24afaf758fcbcb",
+    "url": "/bmarti-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/site/precache-manifest.f05e98ee6af2f92eea099ba231ebde0c.js"
+  "/bmarti-portfolio/precache-manifest.532387b0e6c8dee66c61e033f1301fdb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/site/index.html", {
+workbox.routing.registerNavigationRoute("/bmarti-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
